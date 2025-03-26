b33bbff5ceecbe1af11a10213ad385250fa137d7
TheodorAT <theodor.anderberg.torngren@gmail.com>
2025-03-26 09:30:41 +0100

    Restructured results.

--- scripts/run_pdlp.sh
+++ scripts/run_pdlp.sh
@@ -7,24 +7,25 @@
 # - neos3
 # - karted
 # - qap15 <--- This one should take about 5 seconds. Not more 
-# POSSIBLE ERROR: On "savsched1" we get -nan in relative duality gap, but still solve it for some reason...
-# Maybe we have made an error, or maybe this will be fixed by introducing some curve-breaking?
-# Dual objective becomes: -inf very quickly, possibly due to the large dual objective coefficients?
-instance="chrom1024-7"
-instance_path="${HOME}/lp_benchmark/${instance}.mps" # We need to have already extracted it from mps.gz
+# Very affected fast instances: (in netlib)
+# forplan   (large improvement)
+# bnl2      (large deterioration)
+instance="forplan"
+instance_path="${HOME}/netlib_benchmark/${instance}.mps" # We need to have already extracted it from mps.gz
 
 accuracy="1.0e-4"
-
-kkt_matrix_pass_limit=1000
+save_similarity="true"
+kkt_matrix_pass_limit=100000
 major_iteration_frequency=40
 verbosity=2
 
-similarity_scaling="true"
+similarity_scaling="false"
+similarity_threshold="0.9"
 steering_vector_option="RESIDUAL_MOMENTUM"        # Select between: "NO_STEERING_VECTORS", "RESIDUAL_MOMENTUM"
+
 steering_vector_restart_option="STEERING_VECTOR_EVERY_MAJOR_ITERATION"    # Select between: "STEERING_VECTOR_NO_RESTARTS",  
                                                                 # "STEERING_VECTOR_EVERY_MAJOR_ITERATION", "STEERING_VECTOR_EVERY_PDLP_RESTART"
-similarity_threshold="0.9"
-use_feasibility_polishing="false"
+use_feasibility_polishing="true"
 restart_policy="ADAPTIVE_HEURISTIC"                        # Select between: "NO_RESTARTS", "ADAPTIVE_HEURISTIC" 
 step_size_rule="ADAPTIVE_LINESEARCH_RULE"            # Select between: "CONSTANT_STEP_SIZE_RULE", "ADAPTIVE_LINESEARCH_RULE"
 
@@ -60,6 +61,7 @@ params="
     steering_vector_restart_option: ${steering_vector_restart_option},
     similarity_threshold: ${similarity_threshold},
     similarity_scaling: ${similarity_scaling},
+    save_similarity: ${save_similarity}
 "
 
 solve_log_file="${HOME}/MasterThesisCpp/benchmarking_results/solve_logs/log_${experiment_name}.json"
